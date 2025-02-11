57c690e757bda21dcf475ba5a85dd5cc49859131
Rahulstar177 <rathodsachiit@gmaiil.com>
2025-02-11 19:21:12 +0530

    This is very important work

---
+++ src/main/java/com/eurekaserver/ipm/ImportantWork.java
@@ -0,0 +1,11 @@
+package com.eurekaserver.ipm;
+
+public class ImportantWork {
+
+	
+	//this is very important updates we merge it
+	public String myData()
+	{
+		return "Welcome to bikkadIT";
+	}
+}
