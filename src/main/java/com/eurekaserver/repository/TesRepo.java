eea15931d790e05beb882ba1413052592624330c
Rahulstar177 <rathodsachiit@gmaiil.com>
2025-02-11 19:31:03 +0530

    this is second commit

--- src/main/java/com/eurekaserver/repository/TesRepo.java
+++ src/main/java/com/eurekaserver/repository/TesRepo.java
@@ -1,5 +1,14 @@
 package com.eurekaserver.repository;
 
 public class TesRepo {
+	// this is very important updates we merge it
+	public String myData() {
+		return "Welcome to bikkadIT";
+	}
+
+	// this is very important updates we merge it
+	public String myDataFirst() {
+		return "Welcome to bikkadIT";
+	}
 
 }
